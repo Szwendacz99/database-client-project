b1d5e88650ab1b977f10662df14354d3686b492f
Maciej Lebiest <maciejlebiest2@gmail.com>
2021-05-16 16:30:54 +0200

    create basic files/packages structure

---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""
+main file
+start wx framework and launch GUI
+"""
+import logging
+
+logging.basicConfig(format='%(levelname)s :: %(message)s', level=logging.DEBUG)
+
+logging.info('Program starting')
