48315a690d468bc15595a7495d05ce96ddb8b812
Maciej Lebiest <maciejlebiest2@gmail.com>
2021-05-20 16:32:50 +0200

    Qt <3

--- src/gui/main_frame.py
+++ src/gui/main_frame.py
@@ -1,5 +1,6 @@
 import PyQt5.QtCore
-from PyQt5.QtWidgets import QListWidget, QMainWindow, QTabWidget, QGridLayout, QGroupBox, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QListWidget, QMainWindow, QTabWidget, QGridLayout, QGroupBox, QHBoxLayout, QPushButton, \
+    QDockWidget
 import src.settings as settings
 from PyQt5.QtCore import Qt
 
@@ -14,6 +15,7 @@ class MainFrame(QMainWindow):
         self.central_widget = None
         self.tabs = None
         self.table_list = None
+        self.table_list_dockable = None
         self.setup_ui()
 
     def setup_ui(self):
@@ -48,18 +50,31 @@ class MainFrame(QMainWindow):
         layout_buttons.addWidget(QPushButton("button 4"), alignment=Qt.AlignLeft)
 
         box.setLayout(layout_buttons)
-        layout.addWidget(box, 0, 1, 1, 3)
+        layout.addWidget(box, 0, 0, 4, 10)
 
+        self.table_list_dockable = QDockWidget("Tables", self)
         self.table_list = QListWidget(self.central_widget)
         self.table_list.addItem("tab1")
         self.table_list.addItem("tab2")
         self.table_list.addItem("tab3")
         self.table_list.addItem("tab4")
-        layout.addWidget(self.table_list, 0, 0, 10, 1, alignment=Qt.AlignLeft)
+        self.table_list.addItem("tab3")
+        self.table_list.addItem("tab4")
+        self.table_list.addItem("tab3")
+        self.table_list.addItem("tab4")
+        self.table_list.addItem("tab3")
+        self.table_list.addItem("tab4")
+        self.table_list.addItem("tab3")
+        self.table_list.addItem("tab4")
+        self.table_list.addItem("tab3")
+        self.table_list.addItem("tab4")
+        self.table_list_dockable.setWidget(self.table_list)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.table_list_dockable)
+        # layout.addWidget(self.table_list_dockable, 0, 0, 10, 1)
 
         self.tabs = QTabWidget(self.central_widget)
 
-        layout.addWidget(self.tabs, 1, 1, 9, 3)
+        layout.addWidget(self.tabs, 4, 0, 100, 10)
         self.central_widget.setLayout(layout)
         self.setCentralWidget(self.central_widget)
 
